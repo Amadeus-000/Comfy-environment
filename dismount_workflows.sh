f7df0be1a3be9e835ef7b6bec54d4a47eada6b6f
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2025-11-03 10:48:06 +0900

    add scripts and workflow

---
+++ dismount_workflows.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm ComfyUI/user/default/workflows/myworkflows
---
+++ mount_workflows.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s $(pwd)/myworkflows $(pwd)/ComfyUI/user/default/workflows
+ls -la ComfyUI/user/default/workflows/
