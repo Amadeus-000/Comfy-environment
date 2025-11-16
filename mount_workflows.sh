144fbc8077f85efe5fff6e3daaeadbd3496bf1cb
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2025-11-16 13:35:54 +0900

    update

--- mount_workflows.sh
+++ mount_workflows.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir -p ./ComfyUI/user/default/workflows/myworkflows
 ln -s $(pwd)/myworkflows $(pwd)/ComfyUI/user/default/workflows/myworkflows
 ls -la ComfyUI/user/default/workflows/
