4bfc469ee29c006f7d1ded6f83f17314d023543d
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2025-11-16 13:29:34 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 # https://comfyui-wiki.com/ja/install/install-comfyui/install-comfyui-on-windows
 # based on the above website
 
+# Note: Excute this script with "source install.sh" to apply virtual environment to current shell
+
 python -m venv venv
-source ./venv/Scripts/activate
+source ./venv/bin/activate
 pip install comfy-cli
 comfy --install-completion
 comfy --here install
