762698711fd312e5191f0a8106fa9e36b847caa1
Masahiro Harada <39709974+Amadeus-000@users.noreply.github.com>
2026-01-08 14:29:31 +0000

    SHにpythonバージョン変更機能を追加

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+source ./sh/environment/conda.sh $1
 source ./sh/environment/install.sh
+cp ./config.ini ./ComfyUI/custom_nodes/ComfyUI-Manager/config.ini
 bash ./sh/environment/mount.sh
 bash ./sh/environment/launch.sh
-cp ./config.ini ./ComfyUI/custom_nodes/ComfyUI-Manager/config.ini
---
+++ sh/environment/conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [-z "$1"]; then
+    echo "Using default python version"
+else
+    echo "Using python"$1""
+    conda create -n python python="$1"
+    conda activate python
+fi
