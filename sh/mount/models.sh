b86c2b76506921b4ff0b952bb28ccb19ea5395af
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2026-01-09 12:47:12 +0000

    update sh

--- sh/mount/models.sh
+++ sh/mount/models.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-rm -rf /workspace/Comfy-environment/ComfyUI/models/diffusion_models
-ln -s /workspace/models/diffusion_models /workspace/Comfy-environment/ComfyUI/models/diffusion_models
-
-rm -rf /workspace/Comfy-environment/ComfyUI/models/loras
-ln -s /workspace/models/loras /workspace/Comfy-environment/ComfyUI/models/loras
-
-rm -rf /workspace/Comfy-environment/ComfyUI/models/text_encoders
-ln -s /workspace/models/text_encoders /workspace/Comfy-environment/ComfyUI/models/text_encoders
-
-rm -rf /workspace/Comfy-environment/ComfyUI/models/vae
-ln -s /workspace/models/vae /workspace/Comfy-environment/ComfyUI/models/vae
+for dirpath in /workspace/models/*/; do
+   dirname=$(basename "$dirpath")
+   echo "Mounting model directory: $dirname"
+   rm -rf "/workspace/Comfy-environment/ComfyUI/models/$dirname"
+   ln -s "/workspace/models/$dirname" "/workspace/Comfy-environment/ComfyUI/models/$dirname"
+done
