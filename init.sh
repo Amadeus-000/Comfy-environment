2af3924691594efdda24bcab3cfeabdd60d952bd
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2026-01-12 10:13:25 +0000

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+# arg $1: python version (e.g., 3.10.6)
 
 source ./sh/environment/conda.sh $1
 source ./sh/environment/install.sh
 cp ./config.ini ./ComfyUI/custom_nodes/ComfyUI-Manager/config.ini
-bash ./sh/mount/all.sh
-bash ./sh/environment/launch.sh
+
+if [ "$2" == "m"]; then
+   echo "Mounting all models..."
+   bash ./sh/mount/all.sh
+elif [ "$2" == "l"]; then
+   echo "Launching ComfyUI..."
+   bash ./sh/environment/launch.sh
+elif [ "$2" == "ml"]; then
+   echo "Mounting all models and launching ComfyUI..."
+   bash ./sh/mount/all.sh
+   bash ./sh/environment/launch.sh
+fi
--- sh/environment/conda.sh
+++ sh/environment/conda.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [-z "$1"]; then
+if [ -z "$1" ] || [ "$1" == "..." ]; then
     echo "Using default python version"
 else
-    echo "Using python"$1""
+    echo "Using python $1"
     conda create -n python python="$1"
     conda activate python
 fi
