a5643d7c3464c5534c0e889e1f1ef56cdb84d96e
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2026-01-12 11:59:20 +0000

    fix init.sh

--- init.sh
+++ init.sh
@@ -5,13 +5,13 @@ source ./sh/environment/conda.sh $1
 source ./sh/environment/install.sh
 cp ./config.ini ./ComfyUI/custom_nodes/ComfyUI-Manager/config.ini
 
-if [ "$2" == "m"]; then
+if [ "$2" == "m" ]; then
    echo "Mounting all models..."
    bash ./sh/mount/all.sh
-elif [ "$2" == "l"]; then
+elif [ "$2" == "l" ]; then
    echo "Launching ComfyUI..."
    bash ./sh/environment/launch.sh
-elif [ "$2" == "ml"]; then
+elif [ "$2" == "ml" ]; then
    echo "Mounting all models and launching ComfyUI..."
    bash ./sh/mount/all.sh
    bash ./sh/environment/launch.sh
